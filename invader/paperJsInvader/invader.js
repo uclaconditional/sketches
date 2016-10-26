0ce6a3b7a5aac192bfe91ad1d2b9c24f143f6459
Adam Ferriss <aferriss@gmail.com>
2016-10-26 11:30:16 -0700

    jurg invader

--- invader/paperJsInvader/invader.js
+++ invader/paperJsInvader/invader.js
@@ -1,29 +1,63 @@
-var size = 700;
-var u = size / 7.0;
-var bg = new Path.Rectangle([0, 0], [size, size]);
-bg.fillColor = "blue";
-var grp = null;
+//invader code by Matthias Dorfelt
+function matthiasInvader(){
+    var size = 700;
+    var u = size / 7.0;
+    var bg = new Path.Rectangle([0, 0], [size, size]);
+    bg.fillColor = "blue";
+    var grp = null;
 
-window.setInterval(function()
-{   
-    if(grp)
-        grp.remove();
-    
-    grp = new Group();
+    window.setInterval(function()
+    {   
+        if(grp)
+            grp.remove();
         
-    for (var x = 0; x < 3; x++) 
-    {
-        for (var y = 0; y < 5; y++) 
+        grp = new Group();
+            
+        for (var x = 0; x < 3; x++) 
         {
-            if (Math.random() < 0.5) 
+            for (var y = 0; y < 5; y++) 
             {
-                var rct = new Path.Rectangle([x * u, y * u], [u, u]);
-                var rct2 = new Path.Rectangle([size - (u * 3) - x * u, y * u], [u, u]);
-                grp.addChild(rct);
-                grp.addChild(rct2);
+                if (Math.random() < 0.5) 
+                {
+                    var rct = new Path.Rectangle([x * u, y * u], [u, u]);
+                    var rct2 = new Path.Rectangle([size - (u * 3) - x * u, y * u], [u, u]);
+                    grp.addChild(rct);
+                    grp.addChild(rct2);
+                }
             }
         }
+        grp.fillColor = "white";
+        grp.translate(u, u);
+    }, 1000);
+}
+
+//invader code by Jurg Lehni
+function jurgInvader(){
+    var size = 700;
+    var cell = new Size(size / 7);
+    var bg = new Path.Rectangle({
+        size: size,
+        fillColor: 'blue'
+    });
+    var grp = null;
+    var lastTime = 0
+    view.onFrame = function(event) {
+        if (event.time - lastTime >= 1) {
+            lastTime = event.time;
+            if(grp)
+                grp.remove();
+            grp = new Group();
+            for (var x = 1; x < 4; x++) {
+                for (var y = 1; y < 6; y++) {
+                    if (Math.random() < 0.5) {
+                        grp.addChild(new Shape.Rectangle(cell * [x, y], cell));
+                        grp.addChild(new Shape.Rectangle(cell * [6 - x, y], cell));
+                    }
+                }
+            }
+            grp.fillColor = 'white';
+        }
     }
-    grp.fillColor = "white";
-    grp.translate(u, u);
-}, 1000);
+}
+
+
